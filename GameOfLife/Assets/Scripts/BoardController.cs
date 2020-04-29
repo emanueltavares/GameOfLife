1e0643aa7070cfb672d7daadde28c42fd670842e
Emanuel Tavares <emanuelmaues@gmail.com>
2020-04-29 14:25:27 -0300

    Render board

---
+++ GameOfLife/Assets/Scripts/BoardController.cs
@@ -0,0 +1,44 @@
+﻿using EmanuelTavares.GameOfLife.Models;
+using UnityEngine;
+
+namespace EmanuelTavares.GameOfLife.Controllers
+{
+    public class BoardController : MonoBehaviour
+    {
+        // Serialized Fields
+        [SerializeField] private int _numLines;
+        [SerializeField] private int _numColumns;
+        [SerializeField] private float _cellWidth;
+        [SerializeField] private float _cellHeight;
+        [SerializeField] private GameObject _cellPrefab;
+
+        // Private variables
+        private IBoardModel _boardModel;
+
+        protected virtual void OnEnable()
+        {
+            BuildBoard(_numLines, _numColumns, _cellWidth, _cellHeight);
+        }
+
+        private void BuildBoard(int numLines, int numColumns, float cellWidth = 1f, float cellHeight = 1f)
+        {
+            _boardModel = new BoardModel(numLines, numColumns, cellWidth, cellHeight);
+
+            float halfWidth = cellWidth * (numColumns - 1) * 0.5f;
+            float halfHeight = cellHeight * (numLines - 1) * 0.5f;
+            for (int i = 0; i < numLines; i++)
+            {
+                for (int j = 0; j < numColumns; j++)
+                {
+                    GameObject cellInstance = Instantiate(_cellPrefab, transform, true);
+                    cellInstance.transform.localScale = new Vector3(cellWidth, cellHeight, 1f);
+
+                    float x = Mathf.Lerp(-halfWidth, halfWidth, j / (numColumns - 1f));
+                    float y = Mathf.Lerp(halfHeight, -halfHeight, i / (numLines - 1f));
+                    cellInstance.name = string.Format("{0}:{1}", i, j);
+                    cellInstance.transform.localPosition = new Vector3(x, y, transform.localPosition.z);
+                }
+            }
+        }
+    }
+}
---
+++ GameOfLife/Assets/Scripts/BoardModel.cs
@@ -0,0 +1,21 @@
+﻿namespace EmanuelTavares.GameOfLife.Models
+{
+    public class BoardModel : IBoardModel
+    {
+        // Properties
+        public int NumColumns { get; private set; }
+        public int NumLines { get; private set; }
+        public float CellWidth { get; private set; }
+        public float CellHeight { get; private set; }
+        public bool[,] Cells { get; private set; }
+
+        public BoardModel(int numlines, int numColumns, float cellWidth = 1f, float cellHeight = 1f)
+        {
+            NumLines = numlines;
+            NumColumns = numColumns;
+            CellWidth = cellWidth;
+            CellHeight = cellHeight;
+            Cells = new bool[NumLines, NumColumns];
+        }
+    }
+}
